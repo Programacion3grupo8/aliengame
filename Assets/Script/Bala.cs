0f130918386acec73fbb332cb2e6a833aa885142
Johan <johanmartinezcustodio@gmail.com>
2020-10-23 04:35:15 +0400

    BalaController

---
+++ Assets/Script/Bala.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float VelocidadBala;
+    public float Limite;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * VelocidadBala);
+
+        if(transform.position.x >= Limite)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
