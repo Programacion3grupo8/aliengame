e5f9789236018134b50259b2f9e91f3bb1673377
Asos007 <71027692+Asos007@users.noreply.github.com>
2020-10-27 04:22:41 -0400

    Adiccion de Pausa, Menu de pausa, mejor repsuesta de menu principal
    -Se añadio la pausa al juego, con un banner que indica cuando este esta pausado.
    
    -Opcion de salida (Exit) en menu de pausa
    
    -Habilidad de Resume o continuar al presionar (Escape)
    
    -Mejor respuesta en menu principal.

---
+++ Assets/Script/Pausee.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Pausee : MonoBehaviour
+{
+    bool active;
+    Canvas canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            active = !active;
+            canvas.enabled = active;
+            Time.timeScale = (active) ? 0 : 1f;
+        }
+
+    }
+
+
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+
+
+    }
+}
+
